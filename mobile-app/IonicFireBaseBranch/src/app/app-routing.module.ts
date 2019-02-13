47daf7250432aee4cd54f3010617d701e8e62ac6
tijnthielens <s098007@ap.be>
2019-02-13 10:16:55 +0100

    Got Viktor\'s Ionic app as a starting point.

---
+++ mobile-app/IonicFireBaseBranch/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
+    path: 'list',
+    loadChildren: './list/list.module#ListPageModule'
+  },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'register-page', loadChildren: './register-page/register-page.module#RegisterPagePageModule' },
+  { path: 'login-acc', loadChildren: './login-acc/login-acc.module#LoginAccPageModule' }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
