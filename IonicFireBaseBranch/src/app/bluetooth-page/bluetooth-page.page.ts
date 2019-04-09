dbbffcc2a5591d5179d00b053d3795c96e23770f
Tino <s098007@ap.be>
2019-04-09 16:06:26 +0200

    fixed ble read from arduino.

--- IonicFireBaseBranch/src/app/bluetooth-page/bluetooth-page.page.ts
+++ IonicFireBaseBranch/src/app/bluetooth-page/bluetooth-page.page.ts
@@ -14,33 +14,76 @@ export class BluetoothPagePage implements OnInit {
   ngOnInit(): void {
     console.log(this.sleep_data.user);
   }
-  devices:any[] = [];
+  devices: any[] = [];
   statusMessage: string;
-  constructor(public navCtrl: NavController,public toastController: ToastController,private ble: BLE, private ngZone: NgZone, private themeService: ThemeService, private sleep_data: SleepDataServiceService) { }
-  connect(){
-    this.ble.connect('00:15:87:20:AE:DB').subscribe(async peripheralData => {
-      const toast = await this.toastController.create({
-        message: 'Connected',
-        duration: 2000
-      });
-      toast.present();
-       console.log(peripheralData);
-    },
-    peripheralData => {
-      console.log('Disconnect');
-    });
+  data: any;
+  constructor(public navCtrl: NavController, public toastController: ToastController, private ble: BLE, private ngZone: NgZone, private themeService: ThemeService, private sleep_data: SleepDataServiceService) { }
+  connect() {
+    const BLE_MAC_ADDR = '00:15:87:20:AE:DB';
+    const SERVICE_UUID = 'ffe0';
+    const CHARACTERISTIC_UUID = 'ffe1';
+    this.ble.connect(BLE_MAC_ADDR).subscribe(
+      res => {
+        console.log('Response from subscribe on connect');
+        console.log(res);
+        console.log(JSON.stringify(res));
+        this.ble.startNotification(BLE_MAC_ADDR, get64BitUUID(SERVICE_UUID), get64BitUUID(CHARACTERISTIC_UUID)).subscribe((data) => {
+          console.log('DATA ARRIVED');
+          console.log(data);
+          console.log(JSON.stringify(data));
+          var d = new Uint8Array(data);
+          this.data += bytesToString(d);
+          console.log(this.data);
+        }, (error) => {
+          console.log('error on startnotification');
+          console.log(error);
+        })
+        this.ble.read(BLE_MAC_ADDR, get64BitUUID(SERVICE_UUID), get64BitUUID(CHARACTERISTIC_UUID)).then((val) => {
+          this.data += bytesToString(val);
+          console.log('value arrived on ble read.');
+          console.log('raw');
+          console.log(val);
+          console.log('json.stringify');
+          console.log(JSON.stringify(val));
+          console.log('bytesToString');
+          console.log(bytesToString(val));
+        }).catch((error) => {
+          console.log('failed to receive data...');
+          console.log(error);
+        });
+      },
+      error => { console.log('error on connect'); console.log(error); },
+      () => { console.log('connect completed'); });
   }
-  scan(){
+  scan() {
     this.devices = [];
     this.ble.scan([], 5).subscribe(
       device => this.onDeviceDiscovered(device),
     );
   }
-  onDeviceDiscovered(device){
+  onDeviceDiscovered(device) {
     console.log('Discovered ' + JSON.stringify(device, null, 2));
     this.ngZone.run(() => {
       this.devices.push(device);
       console.log(device);
     });
   }
+}
+
+// ASCII only
+export function stringToBytes(string) {
+  var array = new Uint8Array(string.length);
+  for (var i = 0, l = string.length; i < l; i++) {
+    array[i] = string.charCodeAt(i);
+  }
+  return array.buffer;
+}
+
+// ASCII only
+export function bytesToString(buffer) {
+  return String.fromCharCode.apply(null, new Uint8Array(buffer));
+}
+
+export function get64BitUUID(uuid: string) {
+  return `0000${uuid}-0000-1000-8000-00805f9b34fb`;
 }
